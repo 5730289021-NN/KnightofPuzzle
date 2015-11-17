63284d20a65a2995630d4a4cc1bb40f0d5dab726
5731012721-KL <kosatelim@hotmail.com>
2015-11-18 01:13:14 +0700

    Change to static

--- src/LogicGame/Puzzle.java
+++ src/LogicGame/Puzzle.java
@@ -7,10 +7,10 @@ import Random.Randomul;
 public class Puzzle {
 	private int[][] a = new int[5][5];
 	
-	private final int sp = 1;
-	private final int lp = 2;
-	private final int s = 3;
-	private final int sh = 4;
+	private final static int sp = 1;
+	private final static int lp = 2;
+	private final static int s = 3;
+	private final static int sh = 4;
 	
 	public Puzzle(){
 		for (int i=0; i<5;i++){
