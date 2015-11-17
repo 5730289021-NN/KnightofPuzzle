61e89e79eb269b7bc914379174a06cd28df02ed7
5731012721-KL <kosatelim@hotmail.com>
2015-11-18 01:44:49 +0700

    Change if-else logic

--- src/LogicGame/Puzzle.java
+++ src/LogicGame/Puzzle.java
@@ -6,7 +6,7 @@ import java.util.Random;
 import Random.Randomul;
 
 public class Puzzle {
-	private int[][] a = new int[5][5];
+	private int[][] table = new int[5][5];
 	
 	private final static int sp = 1;
 	private final static int lp = 2;
@@ -16,14 +16,13 @@ public class Puzzle {
 	public Puzzle(){
 		for (int i=0; i<5;i++){
 			for(int j=0; j<5;j++){
-				a[i][j] = Randomul.rand(1, 4);
+				table[i][j] = Randomul.rand(1, 4);
 			}
 		}
 	}
 	
-	//test merge
 	public int[][] getTable() {
-		return a;
+		return table;
 	}
 
 }
