63e9c0448338b8d273e6f2474d4a7f9203c62d98
noraw <noraw@10.204.157.138>
2015-11-18 15:32:49 +0700

    Add 3 Screens(Frames)

---
+++ src/frame/IntroFrame.java
@@ -0,0 +1,21 @@
+package frame;
+
+import java.awt.Graphics2D;
+
+public class IntroFrame implements Frame{
+	
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		drawBackGround();
+		draw
+		
+	}
+
+}
