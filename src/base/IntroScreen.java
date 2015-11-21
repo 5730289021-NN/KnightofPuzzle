c0da519b2752d5978b305adc5502e5d2ff95342b
noraw <noraw@DESKTOP-2U32DML>
2015-11-21 17:29:14 +0700

    add bgm

--- src/base/IntroScreen.java
+++ src/base/IntroScreen.java
@@ -1,5 +1,7 @@
 package base;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -11,6 +13,8 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JOptionPane;
 
+import Data.InfoManager;
+import main.Main;
 import render.AnimationManager;
 import res.Resource;
 
@@ -22,7 +26,6 @@ public class IntroScreen extends JComponent{
 	private JButton continueButton;
 	private JButton exitButton;
 	private AnimationManager introbg;
-	private String[] slots = {"Slot 1","Slot 2","Slot 3"};
 
 	public IntroScreen(){
 		super();
@@ -30,8 +33,8 @@ public class IntroScreen extends JComponent{
 		setPreferredSize(new Dimension(GameScreen.WIDTH, GameScreen.HEIGHT));
 		setVisible(true);
 		setBackground(Color.WHITE);
-		setDoubleBuffered(true);
-		introbg = Resource.get("introbg");
+		setDoubleBuffered(false);
+		introbg = Resource.get("introbg2");
 		introbg.loop();
 		playButton = new JButton("PLAY");
 		playButton.addActionListener(new ActionListener() {
@@ -62,28 +65,84 @@ public class IntroScreen extends JComponent{
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		super.paintComponent(g);
-		g.fillRect(0, 0, GameScreen.WIDTH, GameScreen.HEIGHT);
-		introbg.update();
-		g.drawImage(introbg.getCurrentBufferedImage(0),0,0,GameScreen.WIDTH,GameScreen.HEIGHT,null);
+		//g.fillRect(0, 0, GameScreen.WIDTH, GameScreen.HEIGHT);
+		//introbg.update();
 		g.drawImage(introbg.getCurrentBufferedImage(),0,0,GameScreen.WIDTH,GameScreen.HEIGHT,null);
 	}
 	
 	private void showDialog(boolean mode){
 		String s1 = "";
+		String[] slots = new String[InfoManager.MAXSLOT];
 		if(mode)
 		{
 			s1 = "Load";
+			for(int i = 0;i<InfoManager.MAXSLOT;i++)
+			{
+				if(InfoManager.MAX_LEVEL_COMPLETE[i] == 0)
+				{
+					slots[i] = "Empty Slot";
+				}else
+				{
+					slots[i] = "Slot " + (i+1);
+				}
+			}
 		}
 		else
 		{
 			s1 = "Play";
+			for(int i = 0;i<InfoManager.MAXSLOT;i++)
+			{
+				if(InfoManager.MAX_LEVEL_COMPLETE[i] == 0)
+				{
+					slots[i] = "New Slot " + (i+1);;
+				}else
+				{
+					slots[i] = "Slot " + (i+1);
+				}
+			}
 		}
-		String s = (String) JOptionPane.showInputDialog(this,"Choose Slot to " + s1,s1,JOptionPane.PLAIN_MESSAGE,null,slots,"Slot 1");
+		
+		String s = (String) JOptionPane.showInputDialog(this,"Choose Slot to " + s1,s1,JOptionPane.PLAIN_MESSAGE,null,slots,slots[0]);
 		if ((s != null) && (s.length() > 0)) {
-			System.out.println(s);
+			if(mode)//Continue mode
+			{
+				if(s.equals("Empty Slot"))
+				{
+					JOptionPane.showMessageDialog(null, "You cannot load game from Empty slot, Please repick");
+				}
+				else
+				{
+					InfoManager.SELECTED_SLOT = Integer.parseInt(s.substring(5,6)) - 1;
+					System.out.println("Load Slot : " + InfoManager.SELECTED_SLOT);
+					Main.changeGameScreen(Main.SELECTSCREEN);
+				}
+			}else//Play
+			{
+				if(s.contains("New Slot"))
+				{
+					InfoManager.SELECTED_SLOT = Integer.parseInt(s.substring(9,10)) - 1;
+					System.out.println("New Slot : " + InfoManager.SELECTED_SLOT);
+					Main.changeGameScreen(Main.SELECTSCREEN);
+				}
+				else
+				{
+					int ans = JOptionPane.showConfirmDialog(null, "Are you sure do you want to override the save game " + s + " ?", "Dialog", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+					if(ans == JOptionPane.YES_OPTION)
+					{
+						InfoManager.LEVEL_WEAPON[Integer.parseInt(s.substring(5,6)) - 1] = 0;
+						InfoManager.LEVEL_ARMOR[Integer.parseInt(s.substring(5,6)) - 1] = 0;
+						InfoManager.LEVEL_POTION[Integer.parseInt(s.substring(5,6)) - 1] = 0;
+						InfoManager.MAX_LEVEL_COMPLETE[Integer.parseInt(s.substring(5,6)) - 1] = 0;
+						InfoManager.recalculateStat(Integer.parseInt(s.substring(5,6)) - 1);
+						InfoManager.SELECTED_SLOT = Integer.parseInt(s.substring(5,6)) - 1;
+						Main.changeGameScreen(Main.SELECTSCREEN);
+					}
+					
+				}
+			}
 		}else
 		{
-			System.out.println("Error");
+			System.out.println("No slot selected");
 		}
 	}
 
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import java.applet.Applet;
+import java.applet.AudioClip;
 import java.awt.Dimension;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
+import Data.InfoManager;
 import base.GameScreen;
 import base.IntroScreen;
 import base.SelectLevelScreen;
@@ -16,26 +19,37 @@ public class Main {
 	public static final int INTROSCREEN = 1;
 	public static final int SELECTSCREEN = 2;
 	public static final int GAMESCREEN = 5;
-	
+	//
 	private static IntroScreen i;
 	private static SelectLevelScreen sls;
 	private static GameScreen g;
+	private static JFrame frame;
+	static AudioClip bgSound = Applet.newAudioClip(IntroScreen.class.getClassLoader().getResource("sound/thebeat.wav"));
 	
 	public static void main(String[] args) {
 		new Resource();
+		new InfoManager();
+		//hello
 		i = new IntroScreen();
 		sls = new SelectLevelScreen();
 		g = new GameScreen();
-		JFrame frame = new JFrame();
+		frame = new JFrame();
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				bgSound.play();
+			}
+		});
+		t.start();
 		frame.setTitle("Knight of Puzzle");
+		currentScreen = i;
 		changeGameScreen(INTROSCREEN);
-		frame.add(currentScreen);
 		frame.setPreferredSize(new Dimension(
 			GameScreen.WIDTH,
 			GameScreen.HEIGHT
 		));
 		frame.pack();
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		while(true){
 			try{
 				Thread.sleep(20);
@@ -49,24 +63,32 @@ public class Main {
 	
 	public static void changeGameScreen(int Screen)
 	{
+		frame.remove(currentScreen);
+		System.out.print("Change game screen to ");
 		switch(Screen)
 		{
 			case INTROSCREEN:
 			{
+				System.out.println("Intro");
 				currentScreen = i;
 				break;
 			}
 			case SELECTSCREEN:
 			{
+				System.out.println("Select");
 				currentScreen = sls;
 				break;
 			}
 			case GAMESCREEN:
 			{
+				System.out.println("Game");
 				currentScreen = g;
 				break;
 			}
 		}
+		
+		frame.add(currentScreen);
+		frame.pack();
 	}
 
 }
