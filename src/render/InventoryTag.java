0e5320d6a9ca3dc1a69c10bab09ea020927215fb
noraw <noraw@DESKTOP-2U32DML>
2015-11-26 23:14:13 +0700

    Almost Finish Shop

--- src/render/InventoryTag.java
+++ src/render/InventoryTag.java
@@ -1,5 +1,9 @@
 package render;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 public class InventoryTag extends Tag{
 
 	private boolean isEquipped;
@@ -8,6 +12,26 @@ public class InventoryTag extends Tag{
 	public InventoryTag(int type, int rarity, int x, int y) {
 		super(type, rarity, x, y);
 		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void drawTag(Graphics2D g2)
+	{
+		if(!isUnlocked && !isEquipped)
+		{
+			g2.setComposite(AlphaComposite.SrcOver.derive(0f));
+		}else if(isUnlocked && !isEquipped)
+		{
+			g2.setComposite(AlphaComposite.SrcOver.derive(0.5f));
+		}else if(isUnlocked && isEquipped)
+		{
+			g2.setComposite(AlphaComposite.SrcOver.derive(1f));
+		}else
+		{
+			g2.setComposite(AlphaComposite.SrcOver.derive(0f));
+		}
+		g2.drawImage(tagimg, realX, realY, tagWidth, tagHeight, null);
+		g2.drawImage(accimg, realX, realY, tagWidth/2, accimg.getHeight() * 2 * deltax/accimg.getWidth(), null);
+		g2.setComposite(AlphaComposite.SrcOver.derive(1f));
 	}
 
 }
--- src/render/ShopTag.java
+++ src/render/ShopTag.java
@@ -1,13 +1,18 @@
 package render;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Graphics2D;
+
+import base.GameScreen;
+import res.Resource;
 
 public class ShopTag extends Tag{
 	private boolean isBuyable;
 	
 	public ShopTag(int type, int rarity, int x, int y) {
 		super(type, rarity, x, y);
-		isBuyable = true;
+		isBuyable = false;
 	}
 	
 	public void buyTag(){
@@ -19,10 +24,11 @@ public class ShopTag extends Tag{
 	{
 		if(!isBuyable)
 		{
-			
-			g2.drawImage(tagimg, realX, realY, tagWidth, tagHeight, null);
-			g2.drawImage(accimg, realX, realY, tagWidth/2, accimg.getHeight() * 2 * deltax/accimg.getWidth(), null);
-			
+			g2.setColor(Color.BLACK);
+			g2.setComposite(AlphaComposite.SrcOver.derive(0.5f));
 		}
+		g2.drawImage(tagimg, realX, realY, tagWidth, tagHeight, null);
+		g2.drawImage(accimg, realX, realY, tagWidth/2, accimg.getHeight() * 2 * deltax/accimg.getWidth(), null);
+		g2.setComposite(AlphaComposite.SrcOver.derive(1f));
 	}
 }
