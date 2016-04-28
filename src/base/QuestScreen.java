970a1fdf494ae483e9ea3de6bfef6a383e08b307
Puttiphat <Puttiphat@10.201.170.184>
2016-04-28 21:40:41 +0700

    complete calculate rank

--- src/base/QuestScreen.java
+++ src/base/QuestScreen.java
@@ -8,6 +8,7 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 
+import Data.InfoManager;
 import javafx.scene.control.ProgressBar;
 
 public class QuestScreen extends JComponent {
@@ -18,7 +19,7 @@ public class QuestScreen extends JComponent {
 	private double ValueProgress;
 	private boolean isDone = false;
 	private ArrayList<String> ListQuest = new ArrayList<String>();
-	private JList<ListQuest> jl;
+	private JList<JPanel> jl;
 	public QuestScreen(){
 		super();
 		setVisible(true);
@@ -34,7 +35,15 @@ public class QuestScreen extends JComponent {
 		
 	}
 	private void calculate(int x,double ValueProgress,int reward,int RewardComplete){
-		
+		if(isDone == true){
+			ValueProgress += RewardComplete;
+			if(ValueProgress >=100) {
+				ValueProgress = ValueProgress - 100;
+				x++;
+				InfoManager.MONEY[InfoManager.SELECTED_SLOT] += reward;
+				
+			}
+		}
 		
 	}
 }
